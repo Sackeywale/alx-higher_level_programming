6e1355296b184882194356717eed9104b0eb407d
sackeywale <shatta619wale@gmail.com>
2023-04-18 20:18:44 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """define a square"""
 
+
 class Square:
     """Rep a square"""
-
     def __init__(self, size):
         """Initializing a square class
         Args: size - reps the size of the square
         """
-
         self.__size = size
